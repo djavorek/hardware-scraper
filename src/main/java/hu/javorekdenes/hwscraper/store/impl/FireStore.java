16c0a5f34504602d5174f1e798e2da343373b4c1
Javorek Dénes <javorek.denes@gmail.com>
2021-09-16 20:51:32 +0200

    Solving Documents already exists problem

--- src/main/java/hu/javorekdenes/hwscraper/store/impl/FireStore.java
+++ src/main/java/hu/javorekdenes/hwscraper/store/impl/FireStore.java
@@ -1,5 +1,6 @@
 package hu.javorekdenes.hwscraper.store.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.logging.Logger;
@@ -8,7 +9,6 @@ import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.CollectionReference;
 import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.WriteBatch;
 import com.google.cloud.firestore.WriteResult;
 
 import hu.javorekdenes.hwscraper.search.model.Result;
@@ -26,22 +26,26 @@ public class FireStore implements Store {
 
 	@Override
 	public void saveBatch(String collectionName, List<Result> resultsToSave) throws StoreException {
-		WriteBatch batch = this.firestore.batch();
 		CollectionReference collection = this.firestore.collection(collectionName);
+		List<ApiFuture<WriteResult>> createFutures = new ArrayList<>();
 
 		for (Result result : resultsToSave) {
 			DocumentReference document = collection.document(result.getId());
-			batch.create(document, result);
+			createFutures.add(document.create(result));
 		}
 
-		ApiFuture<List<WriteResult>> batchFuture = batch.commit();
-
-		try {
-			batchFuture.get();
-		} catch (InterruptedException | ExecutionException e) {
-			throw new StoreException(e);
+		for (ApiFuture<WriteResult> future : createFutures) {
+			try {
+				future.get();
+				logger.info("New document saved successfully.");
+			} catch (InterruptedException e) {
+				logger.warning("Save ran into interuption");
+				throw new StoreException(e);
+			} catch (ExecutionException e) {
+				// logger.warning("Execution problem, probably document already exists: " + e.getMessage());
+			}
 		}
 
-		logger.info("All results saved.");
+		logger.info("Result save finished.");
 	}
 }
